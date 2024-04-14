af9f549c013fd03090fe0639f2c988f80e2cd604
Vish2802 <166976131+Vish2802@users.noreply.github.com>
2024-04-14 17:53:53 +0530

    #2
    #2 add bit level

--- entropy.py
+++ entropy.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import pathlib as pt
 from math import log2
@@ -13,48 +14,32 @@ def _cl_co():
 
     LUT = [co(i) for i in range(256)]
 
-    return LUT.__getitem__ # fastest way to count bits in a byte
+    return LUT.__getitem__  # fastest way to count bits in a byte
 
 bitcount = _cl_co()
 
 def main(argv=sys.argv):
-    if len(argv) == 1:
-        print("Provide a file")
-        return
+    parser = argparse.ArgumentParser(description='Calculate entropy of a file.')
+    parser.add_argument('file', type=str, help='Path to the file')
+    parser.add_argument('-b', '--bit', action='store_true', help='Calculate bit-level entropy')
 
-    f = pt.Path(argv[1])
+    args = parser.parse_args(argv[1:])
+
+    f = pt.Path(args.file)
 
     tot = 0
     counts = np.zeros(256, dtype=np.uint32)
-    # h = 0
     with f.open("rb") as fp:
         while (b := fp.read(256)):
-            i = -1
-            for i in range(7, len(b), 8):
-                # h += bitcount(b[i]) \
-                # + bitcount(b[i - 1]) \
-                # + bitcount(b[i - 2]) \
-                # + bitcount(b[i - 3]) \
-                # + bitcount(b[i - 4]) \
-                # + bitcount(b[i - 5]) \
-                # + bitcount(b[i - 6]) \
-                # + bitcount(b[i - 7])
-                # tot += 64
-                tot += 8
-                counts[b[i]] += 1
-                counts[b[i - 1]] += 1
-                counts[b[i - 2]] += 1
-                counts[b[i - 3]] += 1
-                counts[b[i - 4]] += 1
-                counts[b[i - 5]] += 1
-                counts[b[i - 6]] += 1
-                counts[b[i - 7]] += 1
-
-            for i in range(i + 1, len(b)):
-                # tot += 8
-                # h += bitcount(b[i])
-                counts[b[i]] += 1
-                tot += 1
+            for i in range(0, len(b), 8):
+                if args.bit:
+                    for j in range(i, i + 8):
+                        counts[b[j]] += 1
+                        tot += 1
+                else:
+                    for j in range(i, i + 8):
+                        counts[b[j]] += 1
+                    tot += 64
 
     probs = counts / tot
     ent = -1 * (probs * np.log2(np.where(probs == 0, np.ones(1), probs))).sum()
@@ -64,19 +49,8 @@ def main(argv=sys.argv):
     print("Entropy per byte: ", ent, "bits or", ent / 8, "bytes")
     print("Entropy of file: ", ent * tot, "bits or", ent * tot / 8, "bytes")
     print("Size of file: ", tot, "bytes")
-    print("Delta: ", tot - ent * tot / 8, "bytes compressable theoritically")
-    print("Best Theoritical Coding ratio: ", 8 / ent)
-
-    # p1 = h / tot
-    # p0 = (tot - h) / tot
-    # print("Probability to be high: ", p1, h, tot)
-
-    # # Realised late, I could have calculated byte entropy and wouldn't need
-    # # bit counting
-    # ent = p1 * (log2(tot) - log2(h)) + p0 * (log2(tot) - log2(tot - h))
-    # print("Informational entropy per bit: ", ent, "bits")
-    # print("Entropy per byte: ", ent * 8, "bits")
-    # print("Entropy of entire file: ", ent * tot, "bits")
+    print("Delta: ", tot - ent * tot / 8, "bytes compressible theoretically")
+    print("Best Theoretical Coding ratio: ", 8 / ent)
 
 if __name__ == "__main__":
     main()
