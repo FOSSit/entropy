7e784dc501285bbbbd4e12b2af3ae47f1486f077
Jeya Surya Shankar B <111894416+Jsuryaboi-08@users.noreply.github.com>
2024-04-14 11:04:07 +0000

    ability to run multiple files

--- entropy.py
+++ entropy.py
@@ -22,61 +22,35 @@ def main(argv=sys.argv):
         print("Provide a file")
         return
 
-    f = pt.Path(argv[1])
+    for file in argv[1:]:
+        f = pt.Path(file)
 
-    tot = 0
-    counts = np.zeros(256, dtype=np.uint32)
-    # h = 0
-    with f.open("rb") as fp:
-        while (b := fp.read(256)):
-            i = -1
-            for i in range(7, len(b), 8):
-                # h += bitcount(b[i]) \
-                # + bitcount(b[i - 1]) \
-                # + bitcount(b[i - 2]) \
-                # + bitcount(b[i - 3]) \
-                # + bitcount(b[i - 4]) \
-                # + bitcount(b[i - 5]) \
-                # + bitcount(b[i - 6]) \
-                # + bitcount(b[i - 7])
-                # tot += 64
-                tot += 8
-                counts[b[i]] += 1
-                counts[b[i - 1]] += 1
-                counts[b[i - 2]] += 1
-                counts[b[i - 3]] += 1
-                counts[b[i - 4]] += 1
-                counts[b[i - 5]] += 1
-                counts[b[i - 6]] += 1
-                counts[b[i - 7]] += 1
+        if not f.exists():
+            print(f"{f} does not exist.")
+            continue
 
-            for i in range(i + 1, len(b)):
-                # tot += 8
-                # h += bitcount(b[i])
-                counts[b[i]] += 1
-                tot += 1
+        if not f.is_file():
+            print(f"{f} is not a valid file.")
+            continue
 
-    probs = counts / tot
-    ent = -1 * (probs * np.log2(np.where(probs == 0, np.ones(1), probs))).sum()
-    if ent == 0: ent = -1 * ent
-    print(probs)
-    print(counts)
-    print("Entropy per byte: ", ent, "bits or", ent / 8, "bytes")
-    print("Entropy of file: ", ent * tot, "bits or", ent * tot / 8, "bytes")
-    print("Size of file: ", tot, "bytes")
-    print("Delta: ", tot - ent * tot / 8, "bytes compressable theoritically")
-    print("Best Theoritical Coding ratio: ", 8 / ent)
+        tot = 0
+        counts = np.zeros(256, dtype=np.uint32)
 
-    # p1 = h / tot
-    # p0 = (tot - h) / tot
-    # print("Probability to be high: ", p1, h, tot)
+        with f.open("rb") as fp:
+            while (b := fp.read(256)):
+                for i in range(len(b)):
+                    counts[b[i]] += 1
+                    tot += 1
 
-    # # Realised late, I could have calculated byte entropy and wouldn't need
-    # # bit counting
-    # ent = p1 * (log2(tot) - log2(h)) + p0 * (log2(tot) - log2(tot - h))
-    # print("Informational entropy per bit: ", ent, "bits")
-    # print("Entropy per byte: ", ent * 8, "bits")
-    # print("Entropy of entire file: ", ent * tot, "bits")
+        probs = counts / tot
+        ent = -1 * (probs * np.log2(np.where(probs == 0, np.ones(1), probs))).sum()
+        if ent == 0: ent = -1 * ent
+        print(f"\nFile: {f}")
+        print("Entropy per byte: ", ent, "bits or", ent / 8, "bytes")
+        print("Entropy of file: ", ent * tot, "bits or", ent * tot / 8, "bytes")
+        print("Size of file: ", tot, "bytes")
+        print("Delta: ", tot - ent * tot / 8, "bytes compressable theoritically")
+        print("Best Theoritical Coding ratio: ", 8 / ent)
 
 if __name__ == "__main__":
     main()
