d4dfacad31ce2a1148cd2f83040b5e2057ef318c
SrinijaG2402 <sriharig2004@gmail.com>
2024-04-14 17:11:00 +0530

    #2

--- entropy.py
+++ entropy.py
@@ -17,29 +17,16 @@ def _cl_co():
 
 bitcount = _cl_co()
 
-def main(argv=sys.argv):
-    if len(argv) == 1:
-        print("Provide a file")
-        return
-
-    f = pt.Path(argv[1])
+def calculate_byte_entropy(file_path):
+    f = pt.Path(file_path)
 
     tot = 0
     counts = np.zeros(256, dtype=np.uint32)
-    # h = 0
+
     with f.open("rb") as fp:
         while (b := fp.read(256)):
             i = -1
             for i in range(7, len(b), 8):
-                # h += bitcount(b[i]) \
-                # + bitcount(b[i - 1]) \
-                # + bitcount(b[i - 2]) \
-                # + bitcount(b[i - 3]) \
-                # + bitcount(b[i - 4]) \
-                # + bitcount(b[i - 5]) \
-                # + bitcount(b[i - 6]) \
-                # + bitcount(b[i - 7])
-                # tot += 64
                 tot += 8
                 counts[b[i]] += 1
                 counts[b[i - 1]] += 1
@@ -51,32 +38,58 @@ def main(argv=sys.argv):
                 counts[b[i - 7]] += 1
 
             for i in range(i + 1, len(b)):
-                # tot += 8
-                # h += bitcount(b[i])
-                counts[b[i]] += 1
                 tot += 1
+                counts[b[i]] += 1
 
     probs = counts / tot
     ent = -1 * (probs * np.log2(np.where(probs == 0, np.ones(1), probs))).sum()
     if ent == 0: ent = -1 * ent
-    print(probs)
-    print(counts)
-    print("Entropy per byte: ", ent, "bits or", ent / 8, "bytes")
-    print("Entropy of file: ", ent * tot, "bits or", ent * tot / 8, "bytes")
-    print("Size of file: ", tot, "bytes")
-    print("Delta: ", tot - ent * tot / 8, "bytes compressable theoritically")
-    print("Best Theoritical Coding ratio: ", 8 / ent)
 
-    # p1 = h / tot
-    # p0 = (tot - h) / tot
-    # print("Probability to be high: ", p1, h, tot)
+    return ent, tot
 
-    # # Realised late, I could have calculated byte entropy and wouldn't need
-    # # bit counting
-    # ent = p1 * (log2(tot) - log2(h)) + p0 * (log2(tot) - log2(tot - h))
-    # print("Informational entropy per bit: ", ent, "bits")
-    # print("Entropy per byte: ", ent * 8, "bits")
-    # print("Entropy of entire file: ", ent * tot, "bits")
+def calculate_bit_entropy(file_path):
+    f = pt.Path(file_path)
+
+    tot = 0
+    h = 0
+
+    with f.open("rb") as fp:
+        while (b := fp.read(1)):
+            tot += 8
+            h += bitcount(b[0])
+
+    p1 = h / tot
+    p0 = (tot - h) / tot
+
+    ent = p1 * (log2(tot) - log2(h)) + p0 * (log2(tot) - log2(tot - h))
+    return ent, tot
+def main(argv=sys.argv):
+    if len(argv) < 3:
+        print("Usage: python script.py <option> <file1> [<file2> ...]")
+        print("Options:")
+        print("    byte: Calculate byte-level entropy")
+        print("    bit: Calculate bit-level informational entropy")
+        return
+
+    option = argv[1]
+    files = argv[2:]
+
+    if option == "byte":
+        calculate_entropy = calculate_byte_entropy
+    elif option == "bit":
+        calculate_entropy = calculate_bit_entropy
+    else:
+        print("Invalid option. Please choose 'byte' or 'bit'.")
+        return
+
+    for file_path in files:
+        ent, file_size = calculate_entropy(file_path)
+        print("\nFile:", file_path)
+        print("Entropy per byte:", ent, "bits or", ent / 8, "bytes")
+        print("Entropy of file:", ent * file_size, "bits or", ent * file_size / 8, "bytes")
+        print("Size of file:", file_size, "bytes")
+        print("Delta:", file_size - ent * file_size / 8, "bytes compressible theoretically")
+        print("Best Theoretical Coding ratio:", 8 / ent)
 
 if __name__ == "__main__":
     main()
