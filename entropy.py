765e1fa40ba0d5360fc74abb1a05c7940351c747
Dhineshkumar0511 <151438465+Dhineshkumar0511@users.noreply.github.com>
2024-04-14 16:19:35 +0530

    Update entropy.py

--- entropy.py
+++ entropy.py
@@ -1,6 +1,5 @@
 import sys
 import pathlib as pt
-from math import log2
 import numpy as np
 
 def _cl_co():
@@ -18,65 +17,41 @@ def _cl_co():
 bitcount = _cl_co()
 
 def main(argv=sys.argv):
-    if len(argv) == 1:
-        print("Provide a file")
+    if len(argv) < 2:
+        print("Usage: python script.py <file> <entropy_type>")
+        print("<entropy_type>: 'byte' or 'bit'")
+        return
+
+    entropy_type = argv[2].lower() if len(argv) >= 3 else 'byte'
+
+    if entropy_type not in ['byte', 'bit']:
+        print("Invalid entropy type. Please choose 'byte' or 'bit'.")
         return
 
     f = pt.Path(argv[1])
 
     tot = 0
     counts = np.zeros(256, dtype=np.uint32)
-    # h = 0
     with f.open("rb") as fp:
         while (b := fp.read(256)):
-            i = -1
-            for i in range(7, len(b), 8):
-                # h += bitcount(b[i]) \
-                # + bitcount(b[i - 1]) \
-                # + bitcount(b[i - 2]) \
-                # + bitcount(b[i - 3]) \
-                # + bitcount(b[i - 4]) \
-                # + bitcount(b[i - 5]) \
-                # + bitcount(b[i - 6]) \
-                # + bitcount(b[i - 7])
-                # tot += 64
-                tot += 8
-                counts[b[i]] += 1
-                counts[b[i - 1]] += 1
-                counts[b[i - 2]] += 1
-                counts[b[i - 3]] += 1
-                counts[b[i - 4]] += 1
-                counts[b[i - 5]] += 1
-                counts[b[i - 6]] += 1
-                counts[b[i - 7]] += 1
-
-            for i in range(i + 1, len(b)):
-                # tot += 8
-                # h += bitcount(b[i])
+            for i in range(len(b)):
                 counts[b[i]] += 1
                 tot += 1
 
-    probs = counts / tot
-    ent = -1 * (probs * np.log2(np.where(probs == 0, np.ones(1), probs))).sum()
+    if entropy_type == 'byte':
+        probs = counts / tot
+        ent = -1 * (probs * np.log2(np.where(probs == 0, np.ones(1), probs))).sum()
+    else:  # Calculate bit-level entropy
+        h = 0
+        for i in range(len(b)):
+            h += bitcount(b[i])
+        p1 = h / (tot * 8)
+        p0 = (tot * 8 - h) / (tot * 8)
+        ent = p1 * np.log2(1 / p1) + p0 * np.log2(1 / p0)
+
     if ent == 0: ent = -1 * ent
-    print(probs)
-    print(counts)
-    print("Entropy per byte: ", ent, "bits or", ent / 8, "bytes")
-    print("Entropy of file: ", ent * tot, "bits or", ent * tot / 8, "bytes")
+    print("Entropy: ", ent, "bits" if entropy_type == 'bit' else "bytes")
     print("Size of file: ", tot, "bytes")
-    print("Delta: ", tot - ent * tot / 8, "bytes compressable theoritically")
-    print("Best Theoritical Coding ratio: ", 8 / ent)
-
-    # p1 = h / tot
-    # p0 = (tot - h) / tot
-    # print("Probability to be high: ", p1, h, tot)
-
-    # # Realised late, I could have calculated byte entropy and wouldn't need
-    # # bit counting
-    # ent = p1 * (log2(tot) - log2(h)) + p0 * (log2(tot) - log2(tot - h))
-    # print("Informational entropy per bit: ", ent, "bits")
-    # print("Entropy per byte: ", ent * 8, "bits")
-    # print("Entropy of entire file: ", ent * tot, "bits")
 
 if __name__ == "__main__":
     main()
